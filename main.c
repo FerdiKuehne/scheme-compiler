808d878b6fb7de45a53785ee3851b0e89889ad3b
kordano <konrad.kuehne@rocketmail.com>
2015-12-11 16:50:45 +0100

    improve

--- main.c
+++ main.c
@@ -3,18 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef (*operator)(int, int);
+typedef double (*operator)(int, int);
 
 int parse_number(char **Ptr);
-int parse_operator(char **Ptr);
+operator parse_operator(char **Ptr);
 double add (int left, int right);
 double sub (int left, int right);
 double mul (int left, int right);
 double divio (int left, int right);
+
+
 void error ();
 
 /* Überprüfen des Operators */
-int parse_operator(char **Ptr) {
+operator parse_operator(char **Ptr) {
     if ((**Ptr) == '+') {
         return add;
     }
@@ -42,7 +44,7 @@ double mul (int left, int right) {
 }
 /* div funk */
 double divio (int left, int right) {
-    return left/right;
+  return left/right;
 }
 /* Numern zusammenzählen */
 int parse_numb(char **Ptr, int number) {
@@ -58,31 +60,59 @@ void error () {
     printf("fehler");
     exit(0);
 }
+ 
 int main()
 {
-    char sArray[] = "+ 16 2 2 2";
-    char *sPtr  = sArray;
-    char *zPtr  = sArray;
+ 
+    char *zPtr  = NULL;
     operator op = NULL;
     int number  = 0;
     int erg     = 0;
+    int buffer_size = 0;
+    int n=0;
+    FILE *fp = fopen("./data", "r");
 
-    puts(sArray);
+    if (fp == NULL) {
+      error();
+    }
 
-    for(;*sPtr !='\0';sPtr++) {
+    fseek(fp, 0, SEEK_END);
+    buffer_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    printf("size of buffer: %i\n", buffer_size);
+    
+    char *content_buffer = malloc(buffer_size);
+
+    if (content_buffer == NULL) {
+      error();
+    }
+    
+    fgets(content_buffer, buffer_size+1, fp);
+
+    printf("buffer content: %s\n",content_buffer);
+
+
+    printf("\nPos. \t read number \t memory ");
+    
+    for(;n < buffer_size ;n++) {
         /* Leerzeichen */
-        if(*sPtr == ' ') {
-            /* Zwischen Zeiger wird auf akuelle pos gesetzt -> wird benötigt um Zahlen zu trennen */
-            zPtr = sPtr;
-           continue;
+        if(*content_buffer == ' ') {
+            printf("\n-----------------------------------");
+            zPtr=content_buffer;
+            content_buffer++;
+        continue;
         }
+      
         /* Operatoren */
         if( op == NULL) {
-            op = parse_operator(&sPtr);
+            op = parse_operator(&content_buffer);
+            content_buffer++;
             continue;
         }
+        
         /* Prozess */
-        if (sPtr == (zPtr+1)) {
+        if( content_buffer == (zPtr+1)) {
             /* Überprüfen ob eine Zahl im Speicher vorhanden ist */
             if(erg == 0) {
                 erg = number;
@@ -94,27 +124,21 @@ int main()
             }
         }
         /* number parse */
-        number = parse_numb(&sPtr,number);
+        
+        number = parse_numb(&content_buffer,number);
+        printf("\n%i \t %d \t\t %d ", n, number, erg);
+        content_buffer++;
     }
 
+    printf("\n-----------------------------------");
+    
     erg = op(erg, number);
 
-    printf("\ndone: %2d \n", erg);
+    printf("\nfinal result: %2d \n", erg);
 
+    free(fp);
+    
     return 0;
-}
-
-
-/* Funktionen um Datei zu öffnen bzw neu anlegen bzw erstellen bzw lesen */
-
-void file_open_funktion () {
-FILE *PTR
-}
-void file_read_funktion () {
-
-}
-void file_write_funktion () {
-
 }
 
 
